c75846594f1edba97847b82caae7e8f846ca4d8d
RinshidKP <rinshid26@gmail.com>
2024-04-01 16:36:47 +0530

    first commit after api implimentation

---
+++ models/userModel.mjs
@@ -0,0 +1,23 @@
+
+import mongoose from 'mongoose';
+
+const { MONGODB_URI } = process.env;
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
+
+export const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(MONGODB_URI);
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+};
---
+++ routes/authRoutes.mjs
@@ -0,0 +1,15 @@
+// authRoutes.mjs
+
+import express from 'express';
+import { authenticateToken } from '../middleware/authMiddleware.mjs';
+import { signup, login, getUserDetails, logout, updateUsername } from '../controllers/authController.mjs';
+
+const router = express.Router();
+
+router.post('/signup', signup);
+router.post('/login', login);
+router.get('/user', authenticateToken,getUserDetails);
+router.put('/update-username', authenticateToken,updateUsername);
+router.post('/logout', logout);
+
+export default router;
